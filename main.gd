6c221b6f911f71835acd97b8a7174e1c51fb63b8
divyendu <divyendua@gmail.com>
2025-08-03 16:58:41 -0700

    update to support latest screen sizes

--- main.gd
+++ main.gd
@@ -4,6 +4,7 @@ func _ready():
 	var level = load("res://level/level.tscn")
 	var level_inst = level.instantiate()
 	level_inst.position = Vector2(0, 0)
+	level_inst.scale = Vector2(1.2, 1.2) # Set scale to 2x
 	add_child(level_inst)
 
 func _process(_delta):
